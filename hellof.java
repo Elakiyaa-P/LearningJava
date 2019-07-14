1a28c43962e924d40d7e9e8316dc6536fdd25a78
Elakiyaa-P <44283947+Elakiyaa-P@users.noreply.github.com>
2019-07-14 12:18:42 +0530

    Add files via upload

---
+++ hellof.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class hellof
+{	public static void main(String a[])
+	{	hello();
+	}
+	private static void hello()
+	{	System.out.println("Hello World");
+	}
+}
+
+	
---
+++ stringret.java
@@ -0,0 +1,9 @@
+class stringret
+{	public static void main(String args[])
+	{	System.out.println(hello());		
+	}
+	static String hello()
+	{	String str="Hello World";
+		return str;
+	}
+}
